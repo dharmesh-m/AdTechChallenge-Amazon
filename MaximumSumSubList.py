5e0a9f251cb352602199f8ba2b73b8f8e14985ed
unknown <dguna@20EX20729D.vmware.com>
2016-03-19 14:12:06 +0530

    MaximumSum Problem Check-in

---
+++ MaximumSumSubList.py
@@ -0,0 +1,20 @@
+gsum = None
+
+def getMaxSumHelper(ar, index, acc):
+    global gsum
+    if index >= len(ar):
+        return    
+    acc = acc + ar[index]
+    if gsum == None or acc > gsum:
+        gsum = acc
+    for i in xrange(index + 2, len(ar)):
+        getMaxSumHelper(ar, i, acc)
+
+def getMaxSum(ar):
+    for i in range(len(ar)):
+        getMaxSumHelper(ar, i, 0)
+    
+if __name__ == "__main__":
+    ar = [0,-2,-6, -9]
+    getMaxSum(ar)
+    print gsum
